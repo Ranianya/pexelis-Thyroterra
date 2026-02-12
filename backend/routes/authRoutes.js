deef3c9278cc487367232f3ebeb55cf48e8ea333
yasminebcf <yasminebcf21@gmail.com>
2026-02-12 13:58:19 +0000

    auth fix

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,10 @@
 import express from "express";
 import { register, login } from "../controllers/authController.js";
+import { protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
+
+// Only logged-in users can access this
 
 router.post("/register", register);
 router.post("/login", login);
